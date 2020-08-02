d076f833aa87fb0c6426194d299dbefa68082451
Sezoir <joshmottley0@gmail.com>
2020-08-02 21:45:57 +0100

    - Added Mbed dependency configured by: https://github.com/USCRPL/mbed-cmake

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include "mbed.h"
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+BufferedSerial serial(USBTX, USBRX, 115200);
+
+int main() {
+
+    while(true)
+    {
+        char const message[] = "Hello world: 7!\n";
+        serial.write(message, sizeof(message));
+        ThisThread::sleep_for(1000);
+    }
 }

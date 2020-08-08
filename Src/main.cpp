7d964a601fb82cc9d21ed7acebcb57ad3abee92d
Sezoir <joshmottley0@gmail.com>
2020-08-08 23:06:00 +0100

    - Setup simple button/ticker combo in main to test increasing throttle od DShot class.

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,21 +1,36 @@
 #include "mbed.h"
 #include "Engine/DShot.hpp"
 
-//BufferedSerial serial(USBTX, USBRX, 115200);
+BufferedSerial serial(USBTX, USBRX, 115200);
+DigitalIn button(BUTTON1);
+Ticker updater;
 
 int main()
 {
 //    char const creMessage[] = "Creating DShot protocol!\n";
 //    serial.write(creMessage, sizeof(creMessage));
 
+    if (button.is_connected()) {
+        printf("mypin is connected and initialized! \n\r");
+    }
+
+    // Optional: set mode as PullUp/PullDown/PullNone/OpenDrain
+    button.mode(PullNone);
+
     Copter::Engine::DShot proto(1200);
     proto.setup();
-    proto.sendSignal();
+    updater.attach(callback(&proto, &Copter::Engine::DShot::sendSignal), 50us);
 
-//    while(true)
-//    {
+    while(true)
+    {
+        if(button.read())
+            for(int x=0; x<2000; x++)
+                proto.incThrottle(0.005f);
+
+        ThisThread::sleep_for(250);
+
 //        char const message[] = "DShot protocol!\n";
 //        serial.write(message, sizeof(message));
 //        ThisThread::sleep_for(1000);
-//    }
+    }
 }

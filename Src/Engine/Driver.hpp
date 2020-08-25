0ae74c06c9bcab7b66fe07d58495fed557926847
Sezoir <joshmottley0@gmail.com>
2020-08-25 01:34:09 +0100

    - Formatted code structure.

--- Src/Engine/Driver.hpp
+++ Src/Engine/Driver.hpp
@@ -1,20 +1,26 @@
 #pragma once
 #include "units.h"
 
-
 namespace units
 {
     UNIT_ADD(velocity, speed, speeds, sd, units::unit<std::ratio<1>, units::category::velocity_unit>)
     UNIT_ADD(velocity, dshot, dshot, ds, units::unit<std::ratio<10000, 2047>, units::velocity::speed>)
-}
+} // namespace units
 
 namespace Copter::Engine
 {
     class Driver
     {
     public:
-        virtual bool setup() {return true;}
-        virtual void sendSignal(const units::velocity::speed_t & ramp) {}
-    };
-}
+        Driver() = default;
 
+        virtual bool setup()
+        {
+            return true;
+        }
+
+        virtual void sendSignal(const units::velocity::speed_t& ramp)
+        {
+        }
+    };
+} // namespace Copter::Engine

ee9eee8a1aeb1a2ed4625b3543b6a923dc006d13
Sezoir <joshmottley0@gmail.com>
2020-09-20 18:20:56 +0100

    - Fixed AngelExtension not correctly finding roll and pitch angles.

--- Src/Sensors/AngleExtension.hpp
+++ Src/Sensors/AngleExtension.hpp
@@ -43,8 +43,7 @@ namespace Copter::Sensors
             std::array<units::angular_velocity::degrees_per_second_t, 3> gyro = mGyro.readGyro();
 
             // Initialise angle vectors
-            std::array<units::angle::degree_t, 2> accelAngle = {0_deg, 0_deg};       // Roll, Pitch
-            std::array<units::angle::degree_t, 3> gyroAngle = {0_deg, 0_deg, 0_deg}; // Roll, Pitch, Yaw
+            std::array<units::angle::degree_t, 2> accelAngle = {0_deg, 0_deg}; // Roll, Pitch
 
             // Calculate time difference
             auto curTime = units::time::millisecond_t(Kernel::Clock::now().time_since_epoch().count());
@@ -52,19 +51,20 @@ namespace Copter::Sensors
             prevTime = curTime;
 
             // Calculate change in angle for accelerometer
-            accelAngle[0] = units::math::atan2(accel[1], accel[2]);
+            accelAngle[0] = units::math::atan2(
+                accel[1], units::math::sqrt(units::math::pow<2>(accel[0]) + units::math::pow<2>(accel[2])));
             accelAngle[1] = units::math::atan2(
                 accel[0], units::math::sqrt(units::math::pow<2>(accel[1]) + units::math::pow<2>(accel[2])));
 
             // Calculate change in angle for gyro
-            gyroAngle[0] = gyro[0] * diffTime;
-            gyroAngle[1] = gyro[1] * diffTime;
-            gyroAngle[2] = gyro[2] * diffTime;
+            mGyroAngle[0] += gyro[1] * diffTime;
+            mGyroAngle[1] += gyro[0] * diffTime;
+            mGyroAngle[2] += gyro[2] * diffTime;
 
             // Use filter to combine data for returnAngle
-            mRoll = mRollFilter(accelAngle[0], gyroAngle[0]);
-            mPitch = mPitchFilter(accelAngle[1], gyroAngle[1]);
-            mYaw = mYawFilter(gyroAngle[2]);
+            mRoll = mRollFilter(mGyroAngle[0], accelAngle[0]);
+            mPitch = mPitchFilter(mGyroAngle[1], accelAngle[1]);
+            mYaw = mYawFilter(mGyroAngle[2]);
         }
 
     private:
@@ -82,6 +82,9 @@ namespace Copter::Sensors
         units::angle::degree_t mRoll = 0_deg;
         units::angle::degree_t mPitch = 0_deg;
         units::angle::degree_t mYaw = 0_deg;
+
+        // Current angle
+        std::array<units::angle::degree_t, 3> mGyroAngle = {0_deg, 0_deg, 0_deg}; // Roll, Pitch, Yaw
 
         // Previous time
         units::time::millisecond_t prevTime =

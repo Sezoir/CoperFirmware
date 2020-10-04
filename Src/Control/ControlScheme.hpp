b55c4c04b31dbb45464366ab6833190ede54d2a9
Sezoir <joshmottley0@gmail.com>
2020-10-04 20:28:10 +0100

    - Changed virtual functions needed to just an "init" and "update" function. - Added comments.

--- Src/Control/ControlScheme.hpp
+++ Src/Control/ControlScheme.hpp
@@ -8,10 +8,19 @@
 
 namespace Copter::Control
 {
+    /**
+     * @brief Base class for any control scheme to inherit from (e.g: an Self-Level class).
+     * @tparam ControlType: A "units" type which will be controlled by the Controller.
+     */
     template <typename ControlType>
     class ControlScheme
     {
     public:
+        /**
+         * @brief Constructor.
+         * @param motorContainer: Reference to the container of motors.
+         * @param data: Reference to the sensor repository.
+         */
         explicit ControlScheme(Engine::MotorContainer<Engine::MOTOR_PROTOCOL, MOTOR_COUNT>& motorContainer,
                                Sensors::SensorRepository& data)
             : mController(Controller<ControlType>(motorContainer))
@@ -19,10 +28,16 @@ namespace Copter::Control
         {
         }
 
-        virtual units::angle::degree_t getRoll() = 0;
-        virtual units::angle::degree_t getPitch() = 0;
-        virtual units::angle::degree_t getYaw() = 0;
-        virtual units::protocol::speed_t getThrottle() = 0;
+        /**
+         * @brief Initialises the control scheme for whatever it may need (for example it may need to calibrate a
+         * minimum throttle to counteract gravity).
+         */
+        virtual void init() = 0;
+
+        /**
+         * @brief Updates the Controller of the new desired values as well calls the Controller update.
+         */
+        virtual void update() = 0;
 
     protected:
         Controller<ControlType> mController;
